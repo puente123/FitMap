7f9a3af6eeca9ea135db2432130295f2bb7517c0
Osbaldo Puente Cerda <oxp9670@mavs.uta.edu>
2024-04-17 11:13:53 -0500

    set routes for api, get post delete patch

--- Backend/routes/workouts.js
+++ Backend/routes/workouts.js
@@ -1,7 +1,36 @@
 const express = require('express')
 
+//creates a router instance
 const router = express.Router()
 
-router.get('/', () => {})
+//GET all workouts
+router.get('/', (req, res) => {
+    res.json({mssg: 'GET all workouts'})
+})
 
+//GET a single workout
+//id is a dynamic parameter
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'Get single workout'})
+})
+
+//POST a new workout
+router.post('/', (req, res) => {
+    res.json({mssg: 'Post a new workout'})
+})
+
+//DELETE a workout
+router.delete('/:id', (req, res) => {
+    res.json({mssg: 'Delete a workout'})
+})
+
+//UPDATE a workout PUT/PATCH
+router.patch('/:id', (req, res) => {
+    res.json({mssg: 'UPDATE/PATCH a workout'})
+})
+
+
+
+//exports router so other files can use it
 module.exports = router
+
--- Backend/server.js
+++ Backend/server.js
@@ -6,13 +6,19 @@ const workoutRoutes = require('./routes/workouts.js')
 //express app
 const app = express()
 
+
+//Middlewear: functions that have acces to req and res 
+//middlewear//USED FOR POST, PUT, allows to use json body data
+app.use(express.json())
+
 //middlewear
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
-app.use(workoutRoutes)
+//routes
+app.use('/api/workouts', workoutRoutes)
 
 //listen for requests
 app.listen(process.env.PORT, () => {
