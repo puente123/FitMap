0af0063d21d56edd141121d5c0c32471678e8b31
Osbaldo Puente Cerda <oxp9670@mavs.uta.edu>
2024-04-17 19:01:43 -0500

    added workout form on right and edited css code to make it look better

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,7 +1,7 @@
 const WorkoutDetails = ({ workout }) => {
 
     return(
-        <div className = "workout_details">
+        <div className = "workout-details">
             <h4>{workout.title}</h4>
             <p><strong>Load (kg): </strong>{workout.load}</p>
             <p><strong>Reps: </strong>{workout.reps}</p>
