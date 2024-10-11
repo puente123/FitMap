7f564e1cacca58814717c5c7b559eb3fa5146400
Osbaldo Puente Cerda <oxp9670@mavs.uta.edu>
2024-10-11 01:58:21 -0500

    Implemented Signup and Login pages into App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 //pages & components
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import SignupPage from './pages/SignupPage'
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<SignupPage/>}/>
+            <Route path="/login" element={<LoginPage/>}/>
           </Routes>
         </div>
       </BrowserRouter>
