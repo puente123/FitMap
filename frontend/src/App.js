9b42674f205a264f99e9b9f03434ee9bbbb6a14d
Osbaldo Puente Cerda <oxp9670@mavs.uta.edu>
2024-10-16 23:30:47 -0500

    updated App.js for routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,33 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import useAuthContext from "./hooks/useAuthorizationContext";
 
 //pages & components
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
-import SignupPage from './pages/SignupPage'
+import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
+  const { user } = useAuthContext();
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<SignupPage/>}/>
-            <Route path="/login" element={<LoginPage/>}/>
+            <Route
+              path="/"
+              element={user ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/signup"
+              element={!user ? <SignupPage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/login"
+              element={!user ? <LoginPage /> : <Navigate to="/" />}
+            />
           </Routes>
         </div>
       </BrowserRouter>
